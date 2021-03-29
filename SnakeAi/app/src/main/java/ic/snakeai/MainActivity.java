ece659d10ae4e936007eaccc643dbd48d40725ea
Denis Gruia <denis.gruiax@outlook.com>
2021-03-29 16:22:02 +0300

    new adequate identification cards

--- SnakeAi/app/src/main/java/ic/snakeai/MainActivity.java
+++ SnakeAi/app/src/main/java/ic/snakeai/MainActivity.java
@@ -4,9 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
