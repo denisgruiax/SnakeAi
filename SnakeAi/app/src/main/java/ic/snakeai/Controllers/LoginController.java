bbbc358560bf123361a6867fc03a0772c2605fb9
Roxana Tomotas <roxanatomotas@yahoo.com>
2021-05-03 21:28:08 +0300

    Update readJson

--- SnakeAi/app/src/main/java/ic/snakeai/Controllers/LoginController.java
+++ SnakeAi/app/src/main/java/ic/snakeai/Controllers/LoginController.java
@@ -1,12 +1,20 @@
 package ic.snakeai.Controllers;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.media.audiofx.DynamicsProcessing;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 
 import Exceptions.EmptyFieldException;
 import Exceptions.IncorrectLoginData;
@@ -19,10 +27,11 @@ public class LoginController {
 
     public static void loginAction(final LoginActivity loginActivity) throws EmptyFieldException, IncorrectLoginData {
 
-        String userName="";
-        String userPassword="";
-        Integer userMaxScore=0;
-
+        String userName = "";
+        String userPassword = "";
+        Integer userMaxScore = 0;
+        ListView listView;
+        ArrayList<UserModel> arrayList;
         String admin_username = "admin";
         String admin_password = "admin";
 
@@ -33,20 +42,15 @@ public class LoginController {
         loginActivity.btn_no_account = loginActivity.findViewById(R.id.btn_no_account);
         loginActivity.btn_back = loginActivity.findViewById(R.id.btn_back);
 
-        if (TextUtils.isEmpty(loginActivity.username.getText().toString()) || TextUtils.isEmpty(loginActivity.password.getText().toString()))
-        {
-
-        }
-        else if (loginActivity.username.getText().toString().equals(admin_username)) {
-            if (loginActivity.password.getText().toString().equals(admin_password)) {
-                {
-
-                }
-            }
-            } else
+        if (TextUtils.isEmpty(loginActivity.username.getText().toString()) || TextUtils.isEmpty(loginActivity.password.getText().toString())) {
+            throw new EmptyFieldException();
+        } else if (loginActivity.username.getText().toString().equals(admin_username)) {
             {
-
+                //admin login
             }
+        } else {
+            //login
+        }
 
         try {
             String pathJSONFile = "users.json";
@@ -58,18 +62,54 @@ public class LoginController {
             userName = user.getString("name");
             userPassword = user.getString("password");
             userMaxScore = user.getInt("maxScore");
-
-            /*
-            userName.setText("Name: "+ userName);
-            userPassword.setText("password: "+userPassword);
-            userMaxScore.setText("maxScore: "+userMaxScore);
-            */
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        UserService.checkEmptyField(userName,userPassword);
+        UserService.checkEmptyField(userName, userPassword);
         UserService.checkUsernameAlreadyExist(userName);
-        UserService.checkLoginCredentials(userName,userPassword);
-        //UserService.updateJSON(loginActivity, user);
+        UserService.checkLoginCredentials(userName, userPassword);
+        arrayList = new ArrayList<>();
+
+        try {
+            JSONObject object = new JSONObject(readJSON());
+            JSONArray array = object.getJSONArray("users");
+            for (int i = 0; i < array.length(); i++) {
+
+                JSONObject jsonObject = array.getJSONObject(i);
+                String username = jsonObject.getString("username");
+                String password = jsonObject.getString("password");
+                Integer score = jsonObject.getInt("score");
+
+                UserModel model = new UserModel();
+                model.setUsername(username);
+                model.setPassword(password);
+                model.setMaxScore(score);
+                arrayList.add(model);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static String readJSON() {
+        String json = null;
+        try {
+            Context context = null;
+            AssetManager manager = context.getAssets();
+            InputStream inputStream = manager.open("users.json");
+            // Opening data.json file
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            // read values in the byte array
+            inputStream.read(buffer);
+            inputStream.close();
+            // convert byte to string
+            json = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return json;
+        }
+        return json;
+    }
+
 }
