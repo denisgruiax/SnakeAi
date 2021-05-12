84a9b233c59dfa03b5e46aff2b034628a7b7c403
Roxana Tomotas <roxanatomotas@yahoo.com>
2021-05-12 21:20:40 +0300

    Update LoginActivity file

--- SnakeAi/app/src/main/java/ic/snakeai/Activities/LoginActivity.java
+++ SnakeAi/app/src/main/java/ic/snakeai/Activities/LoginActivity.java
@@ -1,4 +1,28 @@
 package ic.snakeai.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
 import ic.snakeai.MainActivity;
 import ic.snakeai.R;
 
