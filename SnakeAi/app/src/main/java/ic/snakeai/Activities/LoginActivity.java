662a0adbf7a29d9cb8c6aae8cd864ec7d95aec2a
Roxana Tomotas <roxanatomotas@yahoo.com>
2021-05-12 20:43:14 +0300

    Add LoginActivity file

---
+++ SnakeAi/app/src/main/java/ic/snakeai/Activities/LoginActivity.java
@@ -0,0 +1,18 @@
+package ic.snakeai.Activities;
+
+import ic.snakeai.MainActivity;
+import ic.snakeai.R;
+
+public class LoginActivity extends AppCompatActivity {
+    public Button btn_back;
+
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
