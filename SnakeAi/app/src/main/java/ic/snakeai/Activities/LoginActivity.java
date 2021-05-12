9a021163a2d3a95feec0f1b9f900eb8ba3f90e59
Roxana Tomotas <roxanatomotas@yahoo.com>
2021-05-12 21:16:37 +0300

    Add explanation of functions used

--- SnakeAi/app/src/main/java/ic/snakeai/Activities/LoginActivity.java
+++ SnakeAi/app/src/main/java/ic/snakeai/Activities/LoginActivity.java
@@ -1,18 +1,35 @@
 package ic.snakeai.Activities;
-
 import ic.snakeai.MainActivity;
 import ic.snakeai.R;
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*
+*  The LoginActivity handles the user login in the game.
+*
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 public class LoginActivity extends AppCompatActivity {
+    //Back button from the loginActivity view
     public Button btn_back;
 
+    /* * * * * * * * * * * * * * * * * * * * * * * * ** * * * * * * * * * * * * * * *
+     *
+    * OnStart Function in Android:
+    *   When activity start getting visible to user then onStart() will be called.
+    *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     protected void onStart(){
         super.onStart();
     }
 
+    /* * * * * * * * * * * * * * * * * * * * * * * * ** * * * * * * * * * * ** * * *
+     *
+     * onCreate(Bundle savedInstanceState) Function in Android:
+     *  After Orientation changed then onCreate(Bundle savedInstanceState) will call and recreate the activity and load all data from savedInstanceState.
+     *
+     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
 }
